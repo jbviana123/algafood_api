0c1b78439c8cc62e457e94162eb994d20a724548
jbvianna123 <joaobvianna@hotmail.com>
2022-01-25 23:23:21 -0300

    Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/jbvianna/food/domain/model/Restaurante.java
+++ src/main/java/com/jbvianna/food/domain/model/Restaurante.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Restaurante {
@@ -26,6 +28,7 @@ public class Restaurante {
 	@Column(name="taxa_frete",nullable=false)
 	private BigDecimal taxaFrete;
 	
+//	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cozinha_id",nullable = false)
 	private Cozinha cozinha;
